7aadc263a01ba492f262c514632af7c73b7cf5f8
pcxod <pcxod@273b6b03-9602-4203-8103-cbef60d4472e>
2008-07-27 20:43:01 +0000

    more work on xmodel.h

--- sdl/indexlst.h
+++ sdl/indexlst.h
@@ -5,10 +5,24 @@
 BeginEsdlNamespace()
 // key class, object class
 template <class KC, class OC> class TIndexList {
-  TPSTypeList<KC, OC*> SortedList;
+  struct iEntry {
+    OC* obj;
+    int ind;
+  };
+  TPSTypeList<KC, iEntry> SortedList;
   TTypeList<OC> List;
 public:
   TIndexList() {}
+  TIndexList(const TIndexList& il) {
+    List.SetCapacity( il.Count() );
+    SortedList.SetCapacity( il.Count() );
+    for( int i=0; i < il.Count(); i++ )
+      List.AddCCopy(il[i]);
+    for( int i=0; i < il.Count(); i++ )  {
+      iEntry en = { &List[il.SortedList.Object(i).ind], il.SortedList.Object(i).ind };
+      SortedList.Add( il.SortedList.GetComparable(i), en);
+    }
+  }
   inline OC& operator [] (int i)             {  return List[i];  }
   inline const OC& operator [] (int i) const {  return List[i];  }
   inline int Count()                   const {  return List.Count();  }
@@ -16,11 +30,30 @@ public:
   OC& Last()                                 {  return List.Last();  }
   OC* FindByKey(const KC& key) {  
     int i = SortedList.IndexOfComparable(key);
-    return i == -1 ? NULL : SortedList.Object(i);
+    return i == -1 ? NULL : SortedList.Object(i).obj;
   }
+  inline TIndexList& SetCapacity(int cp)  {
+    List.SetCapacity(cp);
+    SortedList.SetCapacity(cp);
+    return *this;
+  }
+  TIndexList& operator = (const TIndexList& il)  {
+    Clear();
+    List.SetCapacity( il.Count() );
+    SortedList.SetCapacity( il.Count() );
+    for( int i=0; i < il.Count(); i++ )
+      List.AddCCopy(il[i]);
+    for( int i=0; i < il.Count(); i++ )  {
+      iEntry en = { &List[il.SortedList.Object(i).ind], il.SortedList.Object(i).ind };
+      SortedList.Add( il.SortedList.GetComparable(i), en);
+    }
+    return *this;
+  }
+  // takes the ownership of the object - will be deleted
   inline OC& Add(const KC& key, OC* obj)  {
     List.Add(*obj);
-    SortedList.Add(key, obj);
+    iEntry entry = { obj, SortedList.Count() };
+    SortedList.Add(key, entry);
     return *obj;
   }
   inline void Clear()  {
