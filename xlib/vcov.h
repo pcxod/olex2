32e7536f95ecad15095d71b5fc117b68059575e7
pcxod <pcxod@273b6b03-9602-4203-8103-cbef60d4472e>
2008-11-07 18:43:36 +0000

    numerical differentiation added for plane parameters and tetrahedron volume calculations. Precise torsion angle esd added

--- xlib/vcov.h
+++ xlib/vcov.h
@@ -89,7 +89,9 @@ protected:
     }
   }
 public:
-
+  VcoVContainer()  {
+    weights = NULL;
+  }
   void ReadShelxMat(const olxstr& fileName, TAsymmUnit& au) {
     vcov.ReadShelxMat(fileName, au);
   }
@@ -185,9 +187,191 @@ public:
     esd = sqrt(esd)/(v1.Length()*v3.Length()*sin(a)/v2.Length());
     return TEValue<double>(a*180.0/M_PI,esd*180/M_PI);
   }
-
+  // Acta A30, 848, Uri Shmuelli
   TEValue<double> CalcTAng(const TSAtom& a1, const TSAtom& a2, const TSAtom& a3, const TSAtom& a4) {
-    return TEValue<double>(0,0);
+    mat3d_list m;
+    TPtrList<const TCAtom> catoms;
+    TPtrList<const TSAtom> satoms;
+    satoms.Add(&a1);  satoms.Add(&a2);  satoms.Add(&a3);  satoms.Add(&a4);
+    TListCaster::POP(satoms, catoms);
+    vcov.FindVcoV(catoms, m);
+    ProcessSymmetry(satoms, m);
+    mat3d c2f( a1.CAtom().GetParent()->GetCellToCartesian() );
+    mat3d c2f_t( mat3d::Transpose(a1.CAtom().GetParent()->GetCellToCartesian()) );
+    for( int i=0; i < m.Count(); i++ )
+      m[i] = c2f_t*m[i]*c2f;
+    vec3d u(a2.crd()-a1.crd()),
+      v(a3.crd()-a2.crd()),
+      w(a4.crd()-a3.crd());
+    double h11 = u.QLength(),
+      h22 = v.QLength(),
+      h33 = w.QLength(),
+      h12 = u.DotProd(v),
+      h13 = u.DotProd(w),
+      h23 = v.DotProd(w);
+    double A = h12*h23 - h13*h22;
+    double B = h11*h22 - h12*h12;
+    double C = h22*h33 - h23*h23;
+    double tau = acos(A/sqrt(B*C));
+    double K = -1.0/(sqrt(B*C)*sin(tau)), esd = 0;
+    double dt[3][3];
+    for( int i=0; i < 3; i++ )  {
+      dt[i][0] = K*(v[i]*h23-w[i]*h22 - A/(2*B)*2*(u[i]*h22-v[i]*h12));
+      dt[i][1] = K*(u[i]*h23+w[i]*h12-2*v[i]*h13 - A/(2*B)*2*(v[i]*h11-u[i]*h12) -A/(2*C)*2*(v[i]*h33-w[i]*h23));
+      dt[i][2] = K*(v[i]*h12-u[i]*h22 - A/(2*C)*2*(w[i]*h22-v[i]*h23));
+    }
+    double dtx[4][3];
+    for( int i=0; i < 3; i++ )  {
+      dtx[0][i] = -dt[0][i];
+      dtx[1][i] = dt[0][i]-dt[1][i];
+      dtx[2][i] = dt[1][i]-dt[2][i];
+      dtx[3][i] = dt[2][i];
+    }
+    for( int k=0; k < 4; k++ )  {
+      for( int n=0; n < 4; n++ )  {
+        for( int s=0; s < 3; s++ )  {
+          for( int q=0; q < 3; q++ )  {
+            esd += dtx[k][s]*dtx[n][q]*m[k*4+n][s][q];
+          }
+        }
+      }
+    }
+    return TEValue<double>(tau*180.0/M_PI, sqrt(esd)*180.0/M_PI);
+  }
+protected:
+  TDoubleList weights;
+  vec3d plane_params, plane_center;
+  double CalcPlane(const vec3d_list& Points) {
+    mat3d m, vecs;
+    vec3d t;
+    double mass = 0;
+    plane_center.Null();
+    for( int i=0; i < Points.Count(); i++ )  {
+      plane_center += Points[i]*weights[i];
+      mass += weights[i];
+    }
+    plane_center /= mass;
+
+    for( int i=0; i < Points.Count(); i++ )  {
+      vec3d t( Points[i] - plane_center );
+      const double wght = weights[i]*weights[i];
+      m[0][0] += (t[0]*t[0]*wght);
+      m[0][1] += (t[0]*t[1]*wght);
+      m[0][2] += (t[0]*t[2]*wght);
+
+      m[1][1] += (t[1]*t[1]*wght);
+      m[1][2] += (t[1]*t[2]*wght);
+
+      m[2][2] += t[2]*t[2]*wght;
+    } // equ: d = s[0]*x + s[1]*y + s[2]*z
+    m[1][0] = m[0][1];
+    m[2][0] = m[0][2];
+    m[2][1] = m[1][2];
+    mat3d::EigenValues(m, vecs.I());
+    if( m[0][0] < m[1][1] )  {
+      if( m[0][0] < m[2][2] )  {  
+        plane_params = vecs[0];
+        return m[0][0];
+      }
+      else  {
+        plane_params = vecs[2];
+        return m[2][2];
+      }
+    }
+    else  {
+      if( m[1][1] < m[2][2] )  {
+        plane_params = vecs[1];
+        return m[1][1];
+      }
+      else  {
+        plane_params = vecs[2];
+        return m[2][2];
+      }
+    }
+  }
+  double CalcTHV(const vec3d_list& points) const {
+    return TetrahedronVolume(points[0], points[1], points[2], points[3]);
+  }
+  template <class List, class Evaluator> 
+  void CalcDiff(const List& points, TDoubleList& df, Evaluator e)  {
+    const double delta=1.0e-10;
+    for( int i=0; i < points.Count(); i++ )  {
+      for( int j=0; j < 3; j++ )  {
+        points[i][j] += 2*delta;
+        double v1 = (this->*e)(points);
+        points[i][j] -= delta;
+        double v2 = (this->*e)(points);
+        points[i][j] -= 2*delta;
+        double v3 = (this->*e)(points);
+        points[i][j] -= delta;
+        double v4 = (this->*e)(points);
+        df[i*3+j] = (-v1+8*v2-8*v3+v4)/(12*delta);
+        points[i][j] += 2*delta;
+      }
+    }
+  }
+  double CalcEsd(const int sz, const mat3d_list& m, const TDoubleList& df)  {
+    double esd = 0;
+    for( int i=0; i < sz; i++ )  {
+      for( int j=0; j < sz; j++ )  {
+        const int m_ind = i*sz+j;
+        for( int k=0; k < 3; k++ )  {
+          for( int l=0; l < 3; l++ )  {
+            esd += m[m_ind][k][l]*df[i*3+k]*df[j*3+l];
+          }
+        }
+      }
+    }
+    return esd;
+  }
+public:
+  TEValue<double> Plane(const TSAtomPList& atoms) {
+    mat3d_list m;
+    TPtrList<const TCAtom> catoms;
+    TPtrList<const TSAtom> satoms;
+    vec3d_list points;
+    TDoubleList dl(atoms.Count()*3);
+    weights.SetCount(atoms.Count());
+    points.SetCapacity(atoms.Count());
+    satoms.SetCapacity(atoms.Count());
+    for( int i=0; i < atoms.Count(); i++ )  {
+      points.AddNew(atoms[i]->crd());
+      satoms.Add(atoms[i]);
+      weights[i] = 1.0;
+    }
+    TListCaster::POP(satoms, catoms);
+    vcov.FindVcoV(catoms, m);
+    ProcessSymmetry(satoms, m);
+    mat3d c2f( atoms[0]->CAtom().GetParent()->GetCellToCartesian() );
+    mat3d c2f_t( mat3d::Transpose(atoms[0]->CAtom().GetParent()->GetCellToCartesian()) );
+    for( int i=0; i < m.Count(); i++ )
+      m[i] = c2f_t*m[i]*c2f;
+    CalcDiff(points, dl, &VcoVContainer::CalcPlane);
+    double esd = CalcEsd(atoms.Count(), m, dl);
+    return TEValue<double>(CalcPlane(points), sqrt(esd));
+  }
+  TEValue<double> CalcTetrahedronVolume(const TSAtom& a1, const TSAtom& a2, const TSAtom& a3, const TSAtom& a4) {
+    mat3d_list m;
+    TPtrList<const TCAtom> catoms;
+    TPtrList<const TSAtom> satoms;
+    vec3d_list points;
+    satoms.Add(&a1);  satoms.Add(&a2);  satoms.Add(&a3);  satoms.Add(&a4);
+    points.AddCCopy(a1.crd());
+    points.AddCCopy(a2.crd());
+    points.AddCCopy(a3.crd());
+    points.AddCCopy(a4.crd());
+    TDoubleList dl(satoms.Count()*3);
+    TListCaster::POP(satoms, catoms);
+    vcov.FindVcoV(catoms, m);
+    ProcessSymmetry(satoms, m);
+    mat3d c2f( a1.CAtom().GetParent()->GetCellToCartesian() );
+    mat3d c2f_t( mat3d::Transpose(a1.CAtom().GetParent()->GetCellToCartesian()) );
+    for( int i=0; i < m.Count(); i++ )
+      m[i] = c2f_t*m[i]*c2f;
+    vec3d cnt, p;
+    CalcDiff(points, dl, &VcoVContainer::CalcTHV);
+    double esd = CalcEsd(4, m, dl);
+    return TEValue<double>(TetrahedronVolume(points[0], points[1], points[2], points[3]), sqrt(esd));
   }
   const VcoVMatrix& GetMatrix() const {  return vcov;  }
 };
