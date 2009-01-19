d220e7a1504f1fb9a51d70624d0b0433b883fc1f
pcxod <pcxod@273b6b03-9602-4203-8103-cbef60d4472e>
2009-01-19 12:43:47 +0000

    a couple modifications for pict: a crash is fixed when not a number is provided as second argument, jpeg is automatically translated to jpg

--- trunk/olex/mainform.cpp
+++ trunk/olex/mainform.cpp
@@ -2209,7 +2209,7 @@ void TMainForm::OnChar( wxKeyEvent& m )  {
       return;
     }
   }
-  if( (Fl&sssCtrl) && m.GetKeyCode() == 'v'-'a'+1 )  {  // paste
+  if( (Fl&sssCtrl) && m.GetKeyCode() == 'v'-'a'+1 )  {  // paste Ctrl+V
     if( wxTheClipboard->Open() )  {
       if (wxTheClipboard->IsSupported(wxDF_TEXT) )  {
         wxTextDataObject data;
