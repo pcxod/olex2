f1ceeec418a3ec697ceb140a2058819a86a42537
pcxod <pcxod@273b6b03-9602-4203-8103-cbef60d4472e>
2009-03-19 14:38:10 +0000

    implementation of the "synchronous" permutations...

--- branches/dev/sdl/egraph.h
+++ branches/dev/sdl/egraph.h
@@ -20,26 +20,38 @@ protected:
   static int _SortNodesByTag(const NodeType* n1, const NodeType* n2) {
     return n1->GetTag() - n2->GetTag();
   }
-  void GeneratePermutations(const TTypeList<ConnInfo>& conn, TTypeList<TIntList>& res)  {
+public:
+  static void GeneratePermutations(const TTypeList<ConnInfo>& conn, TTypeList<TIntList>& res)  {
     TTypeList<TIntList> op;
     TIntList permutation;
-    int total_perm = 1;
-    for( int i=0; i < conn.Count(); i++ )
+    int total_perm = 1, 
+        total_perm_size = 0,
+        perm_size = 0, 
+        group_size = 1;
+    for( int i=0; i < conn.Count(); i++ )  {
       total_perm *= (int)Factorial(conn[i].GetA().Count());
+      total_perm_size += conn[i].GetA().Count();
+    }
     // precreate the lists
     for( int i=0; i < total_perm; i++ )
-      res.AddNew();
+      res.AddNew(total_perm_size);
     for( int i=0; i < conn.Count(); i++ )  {
       const ConnInfo& ci = conn[i];
       const int perm_cnt = (int)Factorial(ci.GetA().Count());
-      total_perm /= perm_count;
-      int list_ind = 0;
+      const int repeat_n = total_perm/(perm_cnt*group_size);
+      for( int j=1; j < perm_cnt; j++ )
+        for( int k=0; k < group_size; k++ )
+          for( int l=0; l < perm_size; l++ )
+            res[k+j*group_size][l] = res[k][l];
       for( int j=0; j < perm_cnt; j++ )  {
         permutation = ci.GetA();
         GeneratePermutation(permutation, j);
-        for( int k=0; k < total_perm; k++ )
-          res[list_ind++].AddList(permutation);
+        for( int k=0; k < group_size; k++ )
+          for( int l=0; l < permutation.Count(); l++ )
+            res[j*group_size+k][perm_size+l] = permutation[l];
       }
+      group_size *= perm_cnt;
+      perm_size += ci.GetA().Count();
     }
   }
 public:
@@ -203,37 +215,60 @@ public:
       return true;
     }
     TPtrList<TEGraphNode> ond(node.Nodes);
-    TTypeList<TIntList> permutations;
-    GeneratePermutations(conn, permutations);
-    for( int i=0; i < permutations.Count(); i++ )  {
-      const ConnInfo& ci = conn[i];
+    TIntList permutation;
+    if( conn.Count() == 1 )  {
+      const ConnInfo& ci = conn[0];
       const int perm_cnt = (int)Factorial(ci.GetA().Count());
-      int best_perm = -1;
-      double minRms = 0;
-      for( int j=0; j < perm_cnt; j++ )  {
+      int best_perm = 0;
+      double minRms = analyser.CalcRMS();
+      for( int i=1; i < perm_cnt; i++ )  {
         permutation = ci.GetA();
-        GeneratePermutation(permutation, j);
-        for( int k=0; k < permutation.Count(); k++ )
-          node.Nodes[ci.GetA()[k]] = ond[permutation[k]];
-        for( int k=0; k < permutation.Count(); k++ )
-          Nodes[ci.GetA()[k]]->FullMatchEx(node[ci.GetA()[k]], analyser, true);
+        GeneratePermutation(permutation, i);
+        for( int j=0; j < permutation.Count(); j++ )
+          node.Nodes[ci.GetA()[j]] = ond[permutation[j]];
+        for( int j=0; j < permutation.Count(); j++ )
+          Nodes[ci.GetA()[j]]->FullMatchEx(node[ci.GetA()[j]], analyser, true);
         const double rms = analyser.CalcRMS();
-        if( j == 0 )  {
+        if( rms < minRms )  {
           minRms = rms;
-          best_perm = j;
-        }
-        else if( rms < minRms )  {
-          minRms = rms;
-          best_perm = j;
+          best_perm = i;
         }
       }
       if( best_perm != perm_cnt-1 )  {
         permutation = ci.GetA();
         GeneratePermutation(permutation, best_perm);
-        for( int k=0; k < permutation.Count(); k++ )
-          node.Nodes[ci.GetA()[k]] = ond[permutation[k]];
-        for( int k=0; k < node_cnt; k++ )
-          Nodes[k]->FullMatchEx(node[k], analyser, true);
+        for( int j=0; j < permutation.Count(); j++ )
+          node.Nodes[ci.GetA()[j]] = ond[permutation[j]];
+        for( int j=0; j < node_cnt; j++ )
+          Nodes[j]->FullMatchEx(node[j], analyser, true);
+      }
+    }
+    else  {
+      TTypeList<TIntList> permutations;
+      GeneratePermutations(conn, permutations);
+      const int perm_cnt = permutations.Count();
+      const TIntList& original_perm = permutations[0];
+      const int perm_size = permutations[0].Count();
+      int best_perm = 0;
+      double minRms = analyser.CalcRMS();
+      for( int i=1; i < permutations.Count(); i++ )  {
+        const TIntList& permutation = permutations[i];
+        for( int j=0; j < perm_size; j++ )
+          node.Nodes[original_perm[j]] = ond[permutation[j]];
+        for( int j=0; j < perm_size; j++ )
+          Nodes[original_perm[j]]->FullMatchEx(node[original_perm[j]], analyser, true);
+        const double rms = analyser.CalcRMS();
+        if( rms < minRms )  {
+          minRms = rms;
+          best_perm = i;
+        }
+      }
+      if( best_perm != perm_cnt-1 )  {
+        const TIntList& permutation = permutations[best_perm];
+        for( int j=0; j < permutation.Count(); j++ )
+          node.Nodes[original_perm[j]] = ond[permutation[j]];
+        for( int j=0; j < node_cnt; j++ )
+          Nodes[j]->FullMatchEx(node[j], analyser, true);
       }
     }
     PassedFor = &node;
