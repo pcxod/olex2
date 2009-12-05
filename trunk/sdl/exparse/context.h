d25a7b796ff51ebf29411af0c8178c39856d64b8
pcxod <pcxod@273b6b03-9602-4203-8103-cbef60d4472e>
2009-12-05 11:52:35 +0000

    some more work on exparse, not the non-const expression get precompiled, not evaluated (unless forced by assignment or other operators)

--- trunk/sdl/exparse/context.h
+++ trunk/sdl/exparse/context.h
@@ -6,40 +6,65 @@
 BeginEsdlNamespace()
 
 namespace exparse  {
+  struct context;
+
+  struct VarProxy : public IEvaluable {
+    IEvaluable* value;
+    VarProxy(IEvaluable* _value) : value(_value) {  _value->inc_ref();  }
+    ~VarProxy()  {
+      if( value->dec_ref() == 0 )
+        delete value;
+    }
+    void update_value(IEvaluable* _value)  {
+      if( value == _value )  return;
+      if( value->dec_ref() == 0 )
+        delete value;
+      value = _value;
+      _value->inc_ref();
+    }
+    virtual IEvaluable* _evaluate() const {  return value;  }
+    virtual IEvaluable* find_property(const olxstr& name) {  return value->find_property(name);  }
+    virtual IEvaluable* find_method(const olxstr& name, const struct EvaluableFactory& f,
+      const TPtrList<IEvaluable>& args, IEvaluable* proxy=NULL)
+    {
+      return value->find_method(name, f, args, proxy == NULL ? this : proxy);
+    }
+    virtual cast_operator get_cast_operator(const std::type_info& ti) const {  
+      return value->get_cast_operator(ti);
+    } 
+    virtual bool is_final() const {  return false;  }
+  };
   struct context  {
     LibraryRegistry functions; 
-    olxdict<olxstr, IClassRegistry*, olxstrComparator<false> > classes;
+    //olxdict<std::type_info const*, IClassRegistry*, TPointerPtrComparator> classes;
+    //olxdict<olxstr, IClassRegistry*, olxstrComparator<false> > classes;
     olxdict<olxstr, IEvaluable*, olxstrComparator<true> > consts;
-    olxdict<olxstr, IEvaluable*, olxstrComparator<false> > vars;
+    olxdict<olxstr, VarProxy*, olxstrComparator<false> > vars;
     context* parent;
     context(context* _parent = NULL) : parent(_parent)  {}
     ~context()  {
-      for( size_t i=0; i < classes.Count(); i++ )
-        delete classes.GetValue(i);
+      //for( size_t i=0; i < classes.Count(); i++ )
+      //  delete classes.GetValue(i);
       for( size_t i=0; i < consts.Count(); i++ )
         if( consts.GetValue(i)->dec_ref() == 0 )
           delete consts.GetValue(i);
-      for( size_t i=0; i < vars.Count(); i++ )
+      for( size_t i=0; i < vars.Count(); i++ )  {
         if( vars.GetValue(i)->dec_ref() == 0 )
           delete vars.GetValue(i);
+      }
     }
     void add_var(const olxstr& name, IEvaluable* val, bool replace=false)  {
-      size_t i = vars.IndexOf(name);
+      const size_t ind = vars.IndexOf(name);
       if( replace )  {
-        val->inc_ref();
-        if( i == InvalidIndex )
-          vars.Add(name, val);
-        else  {
-          if( vars.GetValue(i)->dec_ref() == 0 )
-            delete vars.GetValue(i);
-          vars.GetEntry(i).val = val;
-        }
+        if( ind == InvalidIndex )
+          vars.Add(name, new VarProxy(val))->inc_ref();
+        else
+          vars.GetValue(ind)->update_value(val);
       }
       else  {
-        if( i != InvalidIndex )
+        if( ind != InvalidIndex )
           throw TInvalidArgumentException(__OlxSourceInfo, olxstr("duplicated variable: ") << name);
-        val->inc_ref();
-        vars.Add(name, val);
+        vars.Add(name, new VarProxy(val))->inc_ref();
       }
     }
     void add_const(const olxstr& name, IEvaluable* val)  {
@@ -49,7 +74,7 @@ namespace exparse  {
       val->inc_ref();
       consts.Add(name, val);
     }
-    IEvaluable* find_var(const olxstr& name) const  {
+    IEvaluable* find_var(const olxstr& name)  {
       size_t ind = vars.IndexOf(name);
       if( ind != InvalidIndex )  return vars.GetValue(ind);
       context* cx = this->parent;
