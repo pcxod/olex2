fe353a9378737004e5ad14faaf7ccd1744fdf26a
pcxod <pcxod@273b6b03-9602-4203-8103-cbef60d4472e>
2010-03-09 22:41:18 +0000

    problem compiling threads on MAC, hope it is still fine on Linux; the example is ported

--- trunk/cchnp/main.cpp
+++ trunk/cchnp/main.cpp
@@ -8,13 +8,18 @@
 
 
 int main(int argc, char** argv)  {
-  TXApp app(argv[0]);
-  app.GetLog().AddStream(new TOutStream, true);
-  TStrObjList args("C12H22O11 C:40.1 H:6 N:0 H2O CCl3H", ' ');
-  TParamList params;
-  TMacroError me;
-  ABasicFunction* macFitCHN = app.GetLibrary().FindMacro("FitCHN");
-  macFitCHN->Run(args, params, me);
-  //app.GetLog() << me.
-  app.GetLog() << "Hello\n";
+  TOutStream out;
+	puts(argv[0]);
+	try  {
+    TXApp app(TBasicApp::GuessBaseDir(argv[0]));
+    app.GetLog().AddStream(new TOutStream, true);
+    TStrObjList args("C12H22O11 C:40.1 H:6 N:0 H2O CCl3H", ' ');
+    TParamList params;
+    TMacroError me;
+    ABasicFunction* macFitCHN = app.GetLibrary().FindMacro("FitCHN");
+    macFitCHN->Run(args, params, me);
+	}
+	catch(const TExceptionBase& e)  {
+	  out.Writenl(e.GetException()->GetError());
+	}
 }
