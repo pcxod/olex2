a2fa20224412f293353278b09764ce7b59d7cd86
pcxod <pcxod@273b6b03-9602-4203-8103-cbef60d4472e>
2010-03-30 19:24:06 +0000

    deleted the dev branch an the release tag

--- branches/dev/lcells/cplanes.h
+++
@@ -1,8 +0,0 @@
-//---------------------------------------------------------------------------
-
-#ifndef cplanesH
-#define cplanesH
-#include "threex3.h"
-//---------------------------------------------------------------------------
-void _fastcall GetPlane(vec3d_list& Crds, vec3d &Z);
-#endif
