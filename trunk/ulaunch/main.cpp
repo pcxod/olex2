680b69e46e22335b6463adc78989300fc08a8975
pcxod <pcxod@273b6b03-9602-4203-8103-cbef60d4472e>
2010-04-08 13:33:04 +0000

    things look better now, problem with basedir is resolved and system dependent environmment can be set now

--- trunk/ulaunch/main.cpp
+++ trunk/ulaunch/main.cpp
@@ -10,9 +10,8 @@
 #  include <process.h>
 #  define PutEnv _putenv
 #  define GetEnv getenv
-void SetEnv(const olxstr& v)  {
-  putenv(v.u_str());
-}
+void SetEnv(const olxstr& v)  {  putenv(v.u_str());  }
+const char exe_name[] = "olex2.dll";
 #else
 #  include <unistd.h>
 #  define PutEnv putenv
@@ -20,8 +19,13 @@ void SetEnv(const olxstr& v)  {
 void SetEnv(const olxstr& v)  {
   size_t ei = v.IndexOf('=');
   if( ei == InvalidIndex )  return;
-	setenv(v.SubStringTo(ei).u_str(), v.SubStringFrom(ei+1).u_str(), 1);
+    setenv(v.SubStringTo(ei).u_str(), v.SubStringFrom(ei+1).u_str(), 1);
 }
+#  ifdef __MAC__
+const char exe_name[] = "olex2.app";
+#  else
+const char exe_name[] = "olex2";
+#  endif
 #endif
 
 void Launch();
@@ -47,24 +51,29 @@ int main(int argc, char** argv)  {
   puts(argv[0]);
   try  {
     olxstr argv_0 = argv[0];
-		if( argv_0.IndexOf(' ') != InvalidIndex && !argv_0.StartsFrom('"') )
-		argv_0 = olxstr('"') << argv_0 << '"';
-		TBasicApp app(TBasicApp::GuessBaseDir(argv_0));
+    if( argv_0.IndexOf(' ') != InvalidIndex && !argv_0.StartsFrom('"') )
+      argv_0 = olxstr('"') << argv_0 << '"';
+    TBasicApp app(TBasicApp::GuessBaseDir(argv_0));
     app.GetLog().AddStream(new TOutStream, true);
-    olxstr OlexFN(TBasicApp::GetBaseDir()+ "olex2.dll");
+    olxstr OlexFN(TBasicApp::GetBaseDir()+ exe_name);
     const olxstr set_fn = TBasicApp::GetBaseDir()+ "launch.dat";
     if( TEFile::Exists(set_fn) )  {
       TSettingsFile sf(set_fn);
       const olxstr original_bd = TBasicApp::GetBaseDir();
       olxstr base_dir = sf.GetParam("base_dir");
+      TStrList env_toks(sf.GetParam("env"), ',');
 #ifdef __WIN64__
-      TEFile::AddPathDelimeterI(base_dir) << sf.GetParam("windows64_prefix");
+      TEFile::AddPathDelimeterI(base_dir) << sf.GetParam("prefix_win64");
+      env_toks.Strtok(sf.GetParam("env_win64"), ',');
 #elif __WIN32__
-      TEFile::AddPathDelimeterI(base_dir) << sf.GetParam("windows32_prefix");
+      TEFile::AddPathDelimeterI(base_dir) << sf.GetParam("prefix_win32");
+      env_toks.Strtok(sf.GetParam("env_win32"), ',');
 #elif __MAC__
-      TEFile::AddPathDelimeterI(base_dir) << sf.GetParam("mac_prefix");
+      TEFile::AddPathDelimeterI(base_dir) << sf.GetParam("prefix_mac");
+      env_toks.Strtok(sf.GetParam("env_mac"), ',');
 #else
-      TEFile::AddPathDelimeterI(base_dir) << sf.GetParam("linux_prefix");
+      TEFile::AddPathDelimeterI(base_dir) << sf.GetParam("prefix_linux");
+      env_toks.Strtok(sf.GetParam("env_linux"), ',');
 #endif
       if( !TEFile::IsAbsolutePath(base_dir) )  {
         if( base_dir.StartsFrom('.') || base_dir.StartsFrom("..") )
@@ -81,16 +90,20 @@ int main(int argc, char** argv)  {
         else
           data_dir = original_bd + data_dir;
       }
+#ifndef __WIN32__ // speciat treatment for non-windows data dirs...
+      TEFile::AddPathDelimeterI(data_dir) << ".olex2";
+#endif
       if( !TEFile::Exists(data_dir) )  {
         if( !TEFile::MakeDirs(data_dir) )
           throw TFunctionFailedException(__OlxSourceInfo, "Failed to create DATA_DIR");
       }
-      SetEnv(olxstr("OLEX2_DATADIR=") << TEFile::AddPathDelimeterI(data_dir));
-      for( size_t i=0; i < sf.ParamCount(); i++ )  {
-        if( sf.ParamName(i).StartsFrom("env_") )
-          SetEnv(sf.ParamValue(i));
-      }
+      SetEnv(olxstr("OLEX2_DATADIR=") << data_dir);
+
+      for( size_t i=0; i < env_toks.Count(); i++ )
+        SetEnv(env_toks[i]);
     }
+    else if( !TEFile::Exists(OlexFN) )
+      throw TFunctionFailedException(__OlxSourceInfo, "No settings file is provided or Olex2 executable found");
     if( TBasicApp::GetInstance().IsBaseDirWriteable() )  {
       short res = patcher::PatchAPI::DoPatch(NULL, new TUProgress);
       if( res != patcher::papi_OK )  {
@@ -120,18 +133,18 @@ void Launch()  {
   if( _path != NULL )  path = _path;
   path.Insert(bd.SubStringTo(bd.Length()-1) + ':', 0);
   SetEnv(olxstr("PATH=") << path);
-  SetEnv(olxstr("PYTHONHOME=") << TBasicApp::GetBaseDir() << "Python26");
-  // remove all OLEX2_DATADIR and OLEX2_DIR variables
-  SetEnv("OLEX2_DIR=");
   SetEnv("OLEX2_CCTBX_DIR=");
+  SetEnv("OLEX2_DIR=");
 #ifdef __WIN32__
+  SetEnv(olxstr("PYTHONHOME=") << TBasicApp::GetBaseDir() << "Python26");
+  const olxstr cmdl = bf + "olex2.dll";
 #else
 #  ifdef __MAC__
-  olxstr base_dir = bd + "olex2.app/Contents/MacOS/";
+  const olxstr base_dir = bd + "olex2.app/Contents/MacOS/";
   SetEnv(olxstr("OLEX2_DIR=") << base_dir);
   static const olxch* ld_var = "DYLD_LIBRARY_PATH";
 #  else
-  olxstr base_dir = bd;
+  const olxstr base_dir = bd;
   static const olxch* ld_var = "LD_LIBRARY_PATH";
 #  endif
   olxch* _ld_path = GetEnv(ld_var);
@@ -141,15 +154,8 @@ void Launch()  {
   ld_path.Insert(base_dir+"Python26:", 0);
   ld_path.Insert(base_dir+"lib:", 0);
   SetEnv(olxstr(ld_var) << '=' << ld_path);
-  SetEnv(olxstr("PYTHONHOME=") << base_dir << "Python26");
-#endif
-  olxstr cmdl = TBasicApp::GetBaseDir();
-#ifdef __WIN32__
-  cmdl << "olex2.dll";
-#elif __MAC__
-  cmdl << "olex2.app/Contents/MacOS/olex2";
-#else
-  cmdl << "olex2";
+  SetEnv(olxstr("PYTHONHOME=") << base_dir << "Python26/python2.6");
+  const olxstr cmdl = base_dir + "olex2";
 #endif
   TEFile::ChangeDir(bd);
   execl(cmdl.u_str(), cmdl.u_str(), NULL);
