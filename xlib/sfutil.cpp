245630d73e4d31231ad9e43266402a6f5f9b4a9d
pcxod <pcxod@273b6b03-9602-4203-8103-cbef60d4472e>
2010-07-09 15:47:22 +0000

    update of the plugins is fixed;

--- xlib/sfutil.cpp
+++ xlib/sfutil.cpp
@@ -102,8 +102,8 @@ olxstr SFUtil::GetSF(TRefList& refs, TArrayList<compd>& F,
     if( (hInd|kInd|lInd|mfInd|sfInd|aInd|bInd) == InvalidIndex  ) {
       return "list 3 fcf file is expected";
     }
-    refs.SetCapacity( hklLoop->GetTable().RowCount() );
-    F.SetCount( hklLoop->GetTable().RowCount() );
+    refs.SetCapacity(hklLoop->GetTable().RowCount());
+    F.SetCount(hklLoop->GetTable().RowCount());
     for( size_t i=0; i < hklLoop->GetTable().RowCount(); i++ )  {
       TCifRow& row = hklLoop->GetTable()[i];
       TReflection& ref = refs.AddNew(row[hInd].ToInt(), row[kInd].ToInt(), 
@@ -136,6 +136,7 @@ olxstr SFUtil::GetSF(TRefList& refs, TArrayList<compd>& F,
     double av = 0;
     sw.start("Loading/Filtering/Merging HKL");
     TUnitCell::SymSpace sp = xapp.XFile().GetUnitCell().GetSymSpace();
+    
     RefinementModel::HklStat ms =
       xapp.XFile().GetRM().GetFourierRefList<TUnitCell::SymSpace,RefMerger::ShelxMerger>(sp, refs);
     F.SetCount(refs.Count());
