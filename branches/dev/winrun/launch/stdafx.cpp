54437cfb334d67a0771e4b8f019ab257f3ff6922
pcxod <pcxod@273b6b03-9602-4203-8103-cbef60d4472e>
2010-09-30 11:37:52 +0000

    something wrong went with branching... removing dev

--- branches/dev/winrun/launch/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// launch.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
