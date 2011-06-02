347981daf51e0839d55f7260bd7afea452354caf
pcxod <pcxod@273b6b03-9602-4203-8103-cbef60d4472e>
2011-06-02 21:07:55 +0000

    completing the operator set for the non-unicode build... search result formatting changed...

--- trunk/sdl/linked_operators.h
+++ trunk/sdl/linked_operators.h
@@ -38,6 +38,21 @@ public:
   heir_t &operator *= (a_t v) {  return SetValue(GetValue()*v);  }
   heir_t &operator /= (a_t v) {  return SetValue(GetValue()/v);  }
 
+  bool operator == (h_t v) const {  return v == GetValue();  }
+  bool operator != (h_t v) const {  return v != GetValue();  }
+  bool operator > (h_t v) const {  return v > GetValue();  }
+  bool operator >= (h_t v) const {  return v >= GetValue();  }
+  bool operator < (h_t v) const {  return v < GetValue();  }
+  bool operator <= (h_t v) const {  return v <= GetValue();  }
+  h_t operator + (h_t v) const {  return h_t(GetValue()+v);  }
+  h_t operator - (h_t v) const {  return h_t(GetValue()-v);  }
+  h_t operator / (h_t v) const {  return h_t(GetValue()/v);  }
+  h_t operator * (h_t v) const {  return h_t(GetValue()*v);  }
+  heir_t &operator += (h_t v) {  return SetValue(GetValue()+v);  }
+  heir_t &operator -= (h_t v) {  return SetValue(GetValue()-v);  }
+  heir_t &operator *= (h_t v) {  return SetValue(GetValue()*v);  }
+  heir_t &operator /= (h_t v) {  return SetValue(GetValue()/v);  }
+
   template <class aheir_t>
   bool operator == (const linked_operators<a_t, aheir_t, h_t> &v) const {
     return v.GetValue() == GetValue();
