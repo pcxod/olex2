282c3d69a155a871a10cc1d332169de0cbf75ab0
pcxod <pcxod@273b6b03-9602-4203-8103-cbef60d4472e>
2011-06-09 13:57:53 +0000

    gcc compilation

--- trunk/sdl/smart/olx_strcvt.h
+++ trunk/sdl/smart/olx_strcvt.h
@@ -7,45 +7,45 @@ template <typename T> struct olx_str_to_num {
 // int8_t
 template <> struct olx_str_to_num<int8_t> {
   template <class ST, typename TC>
-  int8_t operator ()(const TTSString<ST,TC> &s) const {  return s.ToInt();  }
+  int8_t operator ()(const TTSString<ST,TC> &s) const {  return s.template ToInt();  }
 };
 template <> struct olx_str_to_num<uint8_t> {
   template <class ST, typename TC>
-  uint8_t operator ()(const TTSString<ST,TC> &s) const {  return s.ToUInt();  }
+  uint8_t operator ()(const TTSString<ST,TC> &s) const {  return s.template ToUInt();  }
 };
 // int16_t
 template <> struct olx_str_to_num<int16_t> {
   template <class ST, typename TC>
-  int16_t operator ()(const TTSString<ST,TC> &s) const {  return s.ToInt();  }
+  int16_t operator ()(const TTSString<ST,TC> &s) const {  return s.template ToInt();  }
 };
 template <> struct olx_str_to_num<uint16_t> {
   template <class ST, typename TC>
-  uint16_t operator ()(const TTSString<ST,TC> &s) const {  return s.ToUInt();  }
+  uint16_t operator ()(const TTSString<ST,TC> &s) const {  return s.template ToUInt();  }
 };
 // int32_t
 template <> struct olx_str_to_num<int32_t> {
   template <class ST, typename TC>
-  int32_t operator ()(const TTSString<ST,TC> &s) const {  return s.ToInt();  }
+  int32_t operator ()(const TTSString<ST,TC> &s) const {  return s.template ToInt();  }
 };
 template <> struct olx_str_to_num<uint32_t> {
   template <class ST, typename TC>
-  uint32_t operator ()(const TTSString<ST,TC> &s) const {  return s.ToUInt();  }
+  uint32_t operator ()(const TTSString<ST,TC> &s) const {  return s.template ToUInt();  }
 };
 // int64_t
 template <> struct olx_str_to_num<int64_t> {
   template <class ST, typename TC>
-  int64_t operator ()(const TTSString<ST,TC> &s) const {  return s.ToInt();  }
+  int64_t operator ()(const TTSString<ST,TC> &s) const {  return s.template ToInt();  }
 };
 template <> struct olx_str_to_num<uint64_t> {
   template <class ST, typename TC>
-  uint64_t operator ()(const TTSString<ST,TC> &s) const {  return s.ToUInt();  }
+  uint64_t operator ()(const TTSString<ST,TC> &s) const {  return s.template ToUInt();  }
 };
 // float
 template <> struct olx_str_to_num<float> {
   template <class ST, typename TC>
-  float operator ()(const TTSString<ST,TC> &s) const {  return s.ToFloat<float>();  }
+  float operator ()(const TTSString<ST,TC> &s) const {  return s.template ToFloat<float>();  }
 };
 template <> struct olx_str_to_num<double> {
   template <class ST, typename TC>
-  double operator ()(const TTSString<ST,TC> &s) const {  return s.ToDouble();  }
+  double operator ()(const TTSString<ST,TC> &s) const {  return s.template ToDouble();  }
 };
