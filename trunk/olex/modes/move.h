937d2944e9daa1612e2ab494037d2eb34e5b0af1
x-rayman <x-rayman@273b6b03-9602-4203-8103-cbef60d4472e>
2011-06-22 11:39:58 +0000

    * round 2

--- trunk/olex/modes/move.h
+++ trunk/olex/modes/move.h
@@ -1,10 +1,11 @@
-/*****************************************************************************************
-* Copyright (c) 2004-2011 O. Dolomanov, OlexSys                                          *
-*                                                                                        *
-*  This file is part of the Olex2 Crystallographic refinement package.                   *
-*                                                                                        *
-* This source file is distributed under terms of the licence located in the root folder. *
-******************************************************************************************/
+/******************************************************************************
+* Copyright (c) 2004-2011 O. Dolomanov, OlexSys                               *
+*                                                                             *
+* This file is part of the OlexSys Development Framework.                     *
+*                                                                             *
+* This source file is distributed under the terms of the licence located in   *
+* the root folder.                                                            *
+******************************************************************************/
 
 #ifndef __OLX_MOVE_MODE_H
 #define __OLX_MOVE_MODE_H
