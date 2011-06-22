c0035626833ad03f18f54715800222138634046c
x-rayman <x-rayman@273b6b03-9602-4203-8103-cbef60d4472e>
2011-06-22 11:29:47 +0000

    * Confirming native properties are on and auto for eol

--- sdl/smart/olx_strcvt.h
+++ sdl/smart/olx_strcvt.h
@@ -1,3 +1,12 @@
+/******************************************************************************
+* Copyright (c) 2004-2011 O. Dolomanov, OlexSys                               *
+*                                                                             *
+* This file is part of the OlexSys Development Framework.                     *
+*                                                                             *
+* This source file is distributed under the terms of the licence located in   *
+* the root folder.                                                            *
+******************************************************************************/
+
 // no namespace or headers, to be included in
 // generic
 template <typename T> struct olx_str_to_num {
