4569c00e23e5e6145cb6fb842387d177b0858858
pcxod <pcxod@273b6b03-9602-4203-8103-cbef60d4472e>
2011-10-10 17:02:28 +0000

    updating the scons build script for the sample

--- examples/cchnp/main.cpp
+++ examples/cchnp/main.cpp
@@ -18,8 +18,8 @@
 
 int main(int argc, char** argv)  {
   TOutStream out;
-	puts(argv[0]);
-	try  {
+  puts(argv[0]);
+  try  {
     TXApp app(TBasicApp::GuessBaseDir(argv[0]));
     app.GetLog().AddStream(new TOutStream, true);
     TStrObjList args("C12H22O11 C:40.1 H:6 N:0 H2O CCl3H", ' ');
@@ -27,8 +27,8 @@ int main(int argc, char** argv)  {
     TMacroError me;
     ABasicFunction* macFitCHN = app.GetLibrary().FindMacro("FitCHN");
     macFitCHN->Run(args, params, me);
-	}
-	catch(const TExceptionBase& e)  {
-	  out.Writenl(e.GetException()->GetError());
-	}
+  }
+  catch(const TExceptionBase& e)  {
+    out.Writeln(e.GetException()->GetError());
+  }
 }
