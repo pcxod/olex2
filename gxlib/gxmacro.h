8f46dc000c8997157cdcf478dec84b744ef60f90
pcxod <pcxod@273b6b03-9602-4203-8103-cbef60d4472e>
2012-05-24 14:42:49 +0000

    missing files added...

---
+++ gxlib/gxmacro.h
@@ -0,0 +1,26 @@
+/******************************************************************************
+* Copyright (c) 2004-2012 O. Dolomanov, OlexSys                               *
+*                                                                             *
+* This file is part of the OlexSys Development Framework.                     *
+*                                                                             *
+* This source file is distributed under the terms of the licence located in   *
+* the root folder.                                                            *
+******************************************************************************/
+
+#ifndef __olx_gxlib_xmacro_H
+#define __olx_gxlib_xmacro_H
+#include "gxapp.h"
+
+class GXLibMacros  {
+public:
+  static DefMacro(Grow)
+  static DefMacro(Pack)
+  static DefMacro(Name)
+
+  static DefMacro(Qual)
+
+//  static DefFunc(Lst)
+  static void Export(class TLibrary& lib);
+};
+//---------------------------------------------------------------------------
+#endif
