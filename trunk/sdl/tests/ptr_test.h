593018a97194b152b49bc6ab8819e6d6482eae64
pcxod <pcxod@273b6b03-9602-4203-8103-cbef60d4472e>
2017-01-14 14:13:37 +0000

    more tests... adding the virtual pointer tests

--- trunk/sdl/tests/ptr_test.h
+++ trunk/sdl/tests/ptr_test.h
@@ -8,6 +8,7 @@
 ******************************************************************************/
 
 #include "olxptr.h"
+#include "olxvptr.h"
 
 namespace test {
   class Perishable : public APerishable {
@@ -19,23 +20,78 @@ namespace test {
     Perishable p, p1;
     olx_perishable_ptr<Perishable> ptr(new Perishable);
     delete &ptr();
+    if (ptr.is_valid()) {
+      throw TFunctionFailedException(__OlxSourceInfo, "unexpected result");
+    }
     ptr = new Perishable();
     delete ptr.release();
-    if (!ptr.is_valid()) {
-      Perishable p1;
-      ptr = &p;
-      ptr = &p1;
-      ptr = &p1;
-      ptr = 0;
+    if (ptr.is_valid()) {
+      throw TFunctionFailedException(__OlxSourceInfo, "unexpected result");
     }
     {
-      olx_perishable_ptr<Perishable> ptr_(new Perishable);
-      ptr_ = ptr;
-      ptr.is_valid();
+      Perishable p1_;
+      ptr = &p1_;
+    }
+    if (ptr.is_valid()) {
+      throw TFunctionFailedException(__OlxSourceInfo, "unexpected result");
+    }
+    {
+      ptr = &p;
+      ptr = 0;
+    }
+    if (ptr.is_valid()) {
+      throw TFunctionFailedException(__OlxSourceInfo, "unexpected result");
+    }
+    olx_perishable_ptr<Perishable> ptr_(new Perishable);
+    ptr_ = ptr;
+    if (ptr_.is_valid()) {
+      throw TFunctionFailedException(__OlxSourceInfo, "unexpected result");
     }
   }
   //...................................................................................................
+  struct VPtrTest_O: public IOlxObject {
+    int v;
+    VPtrTest_O(int v) {
+      this->v = v;
+    }
+  };
+  struct VPtr : public olx_virtual_ptr<VPtrTest_O> {
+    VPtrTest_O *inst;
+    VPtr(VPtrTest_O *p) {
+      inst = p;
+    }
+    ~VPtr() {
+      if (inst != 0) {
+        delete inst;
+      }
+    }
+    void set_inst(VPtrTest_O *p) {
+      if (inst != 0) {
+        delete inst;
+        inst = p;
+      }
+    }
+    virtual IOlxObject *get_ptr() const {
+      return inst;
+    }
+
+  };
+  void VptrTest(OlxTests &t) {
+    t.description = __FUNC__;
+    VPtr *ptr = new VPtr(new VPtrTest_O(0));
+    olx_vptr<VPtrTest_O> x(ptr);
+    if (x().v != 0) {
+      throw TFunctionFailedException(__OlxSourceInfo, "unexpected result");
+    }
+    // chnging the underlying object!
+    ptr->set_inst(new VPtrTest_O(1));
+    if (x().v != 1) {
+      throw TFunctionFailedException(__OlxSourceInfo, "unexpected result");
+    }
+  }
+ //...................................................................................................
   void PtrTests(OlxTests &t) {
-    t.Add(&PtrTest1);
+    t.Add(&PtrTest1)
+      .Add(&VptrTest);
   }
 };  //namespace test
