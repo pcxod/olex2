06db2ec3cc1aeacfdc6f81fd7d80593e32439bc6
pcxod <pcxod@273b6b03-9602-4203-8103-cbef60d4472e>
2017-05-09 10:56:45 +0000

    adding optional 'same' restraint to the fitting mode;

--- gxlib/modes/fit.h
+++ gxlib/modes/fit.h
@@ -20,7 +20,7 @@ class TFitMode : public AEventsDispatcher, public AMode {
   TXGroup* group;
   TXAtomPList Atoms, AtomsToMatch;
   vec3d_alist original_crds;
-  bool Initialised, DoSplit;
+  bool Initialised, DoSplit, Restrain;
   int afix, part;
   size_t split_offset;
   class OnUniqHandler : public AActionHandler {
@@ -68,6 +68,19 @@ class TFitMode : public AEventsDispatcher, public AMode {
       TGXApp::GetInstance().XFile().GetLattice().Init();
     }
   };
+  struct idx_pair_t : public olx_pair_t<size_t, size_t> {
+    idx_pair_t(size_t a, size_t b)
+      : olx_pair_t<size_t, size_t>(a, b)
+    {}
+    int Compare(const idx_pair_t &p) const {
+      int d = olx_cmp(a, p.a);
+      if (d == 0) {
+        d = olx_cmp(b, p.b);
+      }
+      return d;
+    }
+  };
+
   double AngleInc;
   OnUniqHandler* uniq_handler;
   TFitModeUndo *undo;
@@ -76,6 +89,7 @@ public:
     : AMode(id),
     Initialised(false),
     DoSplit(false),
+    Restrain(false),
     AngleInc(0),
     undo(0)
   {
@@ -89,6 +103,7 @@ public:
   }
 
   bool Initialise_(TStrObjList& Cmds, const TParamList& Options) {
+    Restrain = Cmds.Containsi("same");
     DoSplit = Options.Contains('s');
     if (DoSplit) {
       split_offset = 0;
@@ -141,7 +156,10 @@ public:
     TAsymmUnit& au = gxapp.XFile().GetAsymmUnit();
     XVar& xv = rm.Vars.NewVar(0.75);
     if (DoSplit) {
-      TCAtomPList to_iso;
+      olxdict<size_t, size_t, TPrimitiveComparator> atom_map;
+      atom_map.SetCapacity(Atoms.Count());
+      olxset<size_t, TPrimitiveComparator> atom_set;
+      atom_set.SetCapacity(Atoms.Count());
       for (size_t i = split_offset; i < Atoms.Count(); i++) {
         if (Atoms[i]->crd().QDistanceTo(original_crds[i]) < 0.01) {
           continue;
@@ -156,6 +174,7 @@ public:
         Atoms[i]->CAtom().SetPart(part);
         // take care of negative parts too
         na.SetPart(olx_sign(part)*(olx_abs(part) + 1));
+        na.SetUiso(Atoms[i]->CAtom().GetUiso());
         // link occupancies
         const double sp = 1. / Atoms[i]->CAtom().GetDegeneracy();
         rm.Vars.AddVarRef(xv, Atoms[i]->CAtom(), catom_var_name_Sof, relation_AsVar, sp);
@@ -180,9 +199,66 @@ public:
         // set coordinates
         na.ccrd() = au.Fractionalise(Atoms[i]->crd());
         Atoms[i]->CAtom().ccrd() = au.Fractionalise(crds[i]);
-        to_iso.Add(Atoms[i]->CAtom());
+        atom_map.Add(Atoms[i]->CAtom().GetId(), na.GetId());
+        atom_set.Add(Atoms[i]->CAtom().GetId());
       }
-      gxapp.XFile().GetLattice().SetAnis(to_iso, false);
+      if (Restrain) {
+        olxset<idx_pair_t, TComparableComparator> bonds12, bonds13;
+        for (size_t i = 0; i < atom_set.Count(); i++) {
+          TCAtom &a = au.GetAtom(atom_set[i]);
+          for (size_t j = 0; j < a.AttachedSiteCount(); j++) {
+            TCAtom::Site &s1 = a.GetAttachedSite(j);
+            if (!s1.matrix.IsFirst() || s1.atom->IsDeleted()) {
+              continue;
+            }
+            size_t a_idx, b_idx;
+            if (a.GetId() > s1.atom->GetId()) {
+              a_idx = s1.atom->GetId();
+              b_idx = a.GetId();
+            }
+            else {
+              a_idx = a.GetId();
+              b_idx = s1.atom->GetId();
+            }
+            bonds12.Add(idx_pair_t(a_idx, b_idx));
+            bool set_atom = atom_set.Contains(s1.atom->GetId());
+            for (size_t k = j+1; k < a.AttachedSiteCount(); k++) {
+              TCAtom::Site &s2 = a.GetAttachedSite(k);
+              if (!s2.matrix.IsFirst() || s2.atom->IsDeleted()) {
+                continue;
+              }
+              // at least one of the atoms should be in the set
+              if (!set_atom && !atom_set.Contains(s2.atom->GetId())) {
+                continue;
+              }
+              if (s1.atom->GetId() > s2.atom->GetId()) {
+                a_idx = s2.atom->GetId();
+                b_idx = s1.atom->GetId();
+              }
+              else {
+                a_idx = s1.atom->GetId();
+                b_idx = s2.atom->GetId();
+              }
+              bonds13.Add(idx_pair_t(a_idx, b_idx));
+            }
+          }
+        }
+        for (size_t i = 0; i < bonds12.Count(); i++) {
+          TSimpleRestraint &sr = rm.rSADI.AddNew();
+          sr.AddAtomPair(au.GetAtom(bonds12[i].a), 0, au.GetAtom(bonds12[i].b), 0);
+          sr.AddAtomPair(au.GetAtom(atom_map.Find(bonds12[i].a, bonds12[i].a)), 0,
+            au.GetAtom(atom_map.Find(bonds12[i].b, bonds12[i].b)), 0);
+        }
+        for (size_t i = 0; i < bonds13.Count(); i++) {
+          TSimpleRestraint &sr = rm.rSADI.AddNew();
+          sr.SetEsd(sr.GetEsd() * 2);
+          sr.AddAtomPair(au.GetAtom(bonds13[i].a), 0, au.GetAtom(bonds13[i].b), 0);
+          sr.AddAtomPair(au.GetAtom(atom_map.Find(bonds13[i].a, bonds13[i].a)), 0,
+            au.GetAtom(atom_map.Find(bonds13[i].b, bonds13[i].b)), 0);
+        }
+      }
+      gxapp.XFile().GetLattice().SetAnis(
+          TCAtomPList(Atoms, FunctionAccessor::MakeConst(&TSAtom::CAtom)), false);
       gxapp.XFile().GetLattice().Uniq();
       gxapp.UpdateDuplicateLabels();
     }
