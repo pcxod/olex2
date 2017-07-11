113a9f3bcccec784a752049ab3fe8a9c2fb1b4fc
pcxod <pcxod@273b6b03-9602-4203-8103-cbef60d4472e>
2017-07-11 17:02:30 +0000

    fixing missing batch number when reading HKL from CIF; fixing bonds disappearing from groups - checking if the matrix is an atom generator rather than just the ids; updating PDB export with floating point Z; some code refactoring;

--- xlib/atomregistry.h
+++ xlib/atomregistry.h
@@ -17,30 +17,30 @@ BeginXlibNamespace()
 template <class, class> class ObjectCaster;
 /* Object provide interface
 */
-template <class obj_t> class TIObjectProvider  {
+template <class obj_t> class TIObjectProvider {
 public:
-  virtual ~TIObjectProvider()  {}
+  virtual ~TIObjectProvider() {}
   virtual size_t Count() const = 0;
   virtual obj_t& New(TNetwork* n) = 0;
   virtual obj_t& Get(size_t i) const = 0;
-  inline obj_t& operator [] (size_t i) const {  return Get(i);  }
-  obj_t& GetLast() const {  return Get(Count()-1);  }
+  inline obj_t& operator [] (size_t i) const { return Get(i); }
+  obj_t& GetLast() const { return Get(Count() - 1); }
   virtual void Delete(size_t i) = 0;
-  inline void DeleteLast()  {  Delete(Count()-1);  }
+  inline void DeleteLast() { Delete(Count() - 1); }
   virtual void Clear() = 0;
   virtual void Null(size_t i) = 0;
   virtual obj_t& Detach(size_t i) = 0;
   virtual obj_t& Attach(obj_t& o) = 0;
   virtual void Pack() = 0;
   virtual void IncCapacity(size_t v) = 0;
-  inline bool IsEmpty() const {  return Count() == 0;  }
+  inline bool IsEmpty() const { return Count() == 0; }
   template <class Functor>
   const TIObjectProvider& ForEach(const Functor& f) const {
-    for( size_t i=0; i < Count(); i++ )
+    for (size_t i = 0; i < Count(); i++)
       f.OnItem(Get(i), i);
     return *this;
   }
-  template <class act_t> ObjectCaster<obj_t, act_t> GetAccessor()  {
+  template <class act_t> ObjectCaster<obj_t, act_t> GetAccessor() {
     return ObjectCaster<obj_t, act_t>(*this);
   }
 };
@@ -53,23 +53,24 @@ template <class obj_t, class act_t> class ObjectCaster
   TIObjectProvider<obj_t>& list;
 protected:
   // dummy function...
-  virtual act_t& New(TNetwork*) {  return *((act_t*)NULL);  }
+  virtual act_t& New(TNetwork*) { return *((act_t*)NULL); }
 public:
-  ObjectCaster(TIObjectProvider<obj_t>& _list) : list(_list)  {}
-  virtual size_t Count() const {  return list.Count();  }
-  virtual act_t& Get(size_t i) const {  return (act_t&)list.Get(i);  }
-  inline act_t& operator [] (size_t i) const {  return Get(i);  }
-  virtual void Delete(size_t i)  {  list.Delete(i);  }
-  virtual void Clear()  {  list.Clear();  }
-  virtual void Null(size_t i)  {  list.Null(i);  }
-  virtual act_t& Detach(size_t i) {  return (act_t&)list.Detach(i);  }
-  virtual act_t& Attach(act_t& o) {  return (act_t&)list.Attach(o);  }
-  virtual void Pack()  {  list.Pack();  }
-  virtual void IncCapacity(size_t v)  {  list.IncCapacity(v);  }
+  ObjectCaster(TIObjectProvider<obj_t>& _list) : list(_list) {}
+  virtual size_t Count() const { return list.Count(); }
+  virtual act_t& Get(size_t i) const { return (act_t&)list.Get(i); }
+  inline act_t& operator [] (size_t i) const { return Get(i); }
+  virtual void Delete(size_t i) { list.Delete(i); }
+  virtual void Clear() { list.Clear(); }
+  virtual void Null(size_t i) { list.Null(i); }
+  virtual act_t& Detach(size_t i) { return (act_t&)list.Detach(i); }
+  virtual act_t& Attach(act_t& o) { return (act_t&)list.Attach(o); }
+  virtual void Pack() { list.Pack(); }
+  virtual void IncCapacity(size_t v) { list.IncCapacity(v); }
   template <class Functor>
   const ObjectCaster& ForEach(const Functor& f) const {
-    for( size_t i=0; i < Count(); i++ )
+    for (size_t i = 0; i < Count(); i++) {
       f.OnItem(Get(i), i);
+    }
     return *this;
   }
 };
@@ -80,43 +81,44 @@ template <class obj_t> class TObjectProvider
 {
 protected:
   TPtrList<obj_t> items;
-  void UpdateOwnerIds()  {
-    for( size_t i=0; i < Count(); i++ )
+  void UpdateOwnerIds() {
+    for (size_t i = 0; i < Count(); i++) {
       Get(i).SetOwnerId(i);
+    }
   }
-  inline obj_t& AddNew(obj_t* o)  {
+  inline obj_t& AddNew(obj_t* o) {
     o->SetOwnerId(Count());
     return *items.Add(o);
   }
 public:
-  virtual size_t Count() const {  return items.Count();  }
-  virtual obj_t& New(TNetwork* n)  {  return AddNew(new obj_t(n));  }
-  virtual obj_t& Get(size_t i) const {  return *items[i];  }
-  inline obj_t& operator [] (size_t i) const {  return Get(i);  }
-  obj_t& GetLast() const {  return *items.GetLast();  }
-  virtual void Delete(size_t i)  {
+  virtual size_t Count() const { return items.Count(); }
+  virtual obj_t& New(TNetwork* n) { return AddNew(new obj_t(n)); }
+  virtual obj_t& Get(size_t i) const { return *items[i]; }
+  inline obj_t& operator [] (size_t i) const { return Get(i); }
+  obj_t& GetLast() const { return *items.GetLast(); }
+  virtual void Delete(size_t i) {
     delete items[i];
     items.Delete(i);
     UpdateOwnerIds();
   }
-  inline void DeleteLast()  {  Delete(Count()-1);  }
-  virtual void Clear()   {  items.DeleteItems(false).Clear();  }
-  virtual void Null(size_t i)  {
+  inline void DeleteLast() { Delete(Count() - 1); }
+  virtual void Clear() { items.DeleteItems(false).Clear(); }
+  virtual void Null(size_t i) {
     delete items[i];
-    items.Set(i, NULL);
+    items.Set(i, 0);
   }
-  virtual obj_t& Detach(size_t i)  {
+  virtual obj_t& Detach(size_t i) {
     obj_t* rv = items[i];
     items.Delete(i);
     return *rv;
   }
-  virtual obj_t& Attach(obj_t& o) {  return *items.Add(o);  }
-  virtual void Pack()  {
+  virtual obj_t& Attach(obj_t& o) { return *items.Add(o); }
+  virtual void Pack() {
     items.Pack();
     UpdateOwnerIds();
   }
-  virtual void IncCapacity(size_t v)  {  items.SetCapacity(items.Count()+v);  }
-  inline bool IsEmpty() const {  return items.IsEmpty();  }
+  virtual void IncCapacity(size_t v) { items.SetCapacity(items.Count() + v); }
+  inline bool IsEmpty() const { return items.IsEmpty(); }
   template <class Functor>
   const TObjectProvider& ForEach(const Functor& f) const {
     items.ForEach(f);
@@ -125,22 +127,22 @@ public:
 };
 /* TSAtom registry - an object for quick locating of atoms using their index in
 the asymmetric unit*/
-class AtomRegistry  {
-  struct DataStruct  {
+class AtomRegistry {
+  struct DataStruct {
     olx_array::TArray3D<TArrayList<TSAtomPList*>*> registry;
     mutable int ref_cnt;
     DataStruct(const vec3i& mind, const vec3i& maxd)
       : registry(mind, maxd), ref_cnt(1) {}
-    ~DataStruct()  {
-      for( size_t i=0; i < registry.Length1(); i++ )  {
-        for( size_t j=0; j < registry.Length2(); j++ )  {
-          for( size_t k=0; k < registry.Length3(); k++ )  {
+    ~DataStruct() {
+      for (size_t i = 0; i < registry.Length1(); i++) {
+        for (size_t j = 0; j < registry.Length2(); j++) {
+          for (size_t k = 0; k < registry.Length3(); k++) {
             TArrayList<TSAtomPList*>* aum_slice = registry.Data[i][j][k];
-            if (aum_slice == NULL) {
+            if (aum_slice == 0) {
               continue;
             }
             for (size_t l = 0; l < aum_slice->Count(); l++) {
-              if ((*aum_slice)[l] != NULL) {
+              if ((*aum_slice)[l] != 0) {
                 delete (*aum_slice)[l];
               }
             }
@@ -155,52 +157,61 @@ protected:
 public:
   typedef olx_array::TArray3D<TArrayList<TSAtomPList*>*> RegistryType;
   //...........................................................................
-  AtomRegistry() : data(NULL) {}
+  AtomRegistry() : data(0) {}
   //...........................................................................
-  AtomRegistry(const AtomRegistry& r) : data(r.data) {  data->ref_cnt++;  }
+  AtomRegistry(const AtomRegistry& r) : data(r.data) { data->ref_cnt++; }
   //...........................................................................
-  RegistryType& Init(const vec3i& mind, const vec3i& maxd)  {
-    if( data != NULL && --data->ref_cnt == 0 )
+  RegistryType& Init(const vec3i& mind, const vec3i& maxd) {
+    if (data != 0 && --data->ref_cnt == 0) {
       delete data;
+    }
     data = new DataStruct(mind, maxd);
     return data->registry;
   }
   //...........................................................................
   void Clear() {
-    if (data != NULL && --data->ref_cnt == 0)
+    if (data != NULL && --data->ref_cnt == 0) {
       delete data;
-    data = NULL;
+    }
+    data = 0;
   }
   //...........................................................................
-  ~AtomRegistry()  {
-    if( data != NULL && --data->ref_cnt  == 0 )
+  ~AtomRegistry() {
+    if (data != 0 && --data->ref_cnt == 0) {
       delete data;
+    }
   }
   //...........................................................................
-  AtomRegistry& operator = (const AtomRegistry& ar)  {
-    if( data != NULL && --data->ref_cnt == 0 )
+  AtomRegistry& operator = (const AtomRegistry& ar) {
+    if (data != 0 && --data->ref_cnt == 0) {
       delete data;
+    }
     data = ar.data;
     return *this;
   }
   //...........................................................................
-  RegistryType& GetRegistry()  {  return data->registry;  }
+  RegistryType& GetRegistry() { return data->registry; }
   //...........................................................................
   ConstPtrList<TSAtom> FindAll(const TCAtom &a) const {
     TSAtomPList rv;
-    for (size_t i=0; i < data->registry.Length1(); i++) {
-      for (size_t j=0; j < data->registry.Length2(); j++) {
-        for (size_t k=0; k < data->registry.Length3(); k++) {
+    for (size_t i = 0; i < data->registry.Length1(); i++) {
+      for (size_t j = 0; j < data->registry.Length2(); j++) {
+        for (size_t k = 0; k < data->registry.Length3(); k++) {
           TArrayList<TSAtomPList*>* aum_slice = data->registry.Data[i][j][k];
-          if (aum_slice == NULL) continue;
-          for (size_t l=0; l < aum_slice->Count(); l++) {
+          if (aum_slice == 0) {
+            continue;
+          }
+          for (size_t l = 0; l < aum_slice->Count(); l++) {
             TSAtomPList *au_slice = (*aum_slice)[l];
-            if (au_slice == NULL) continue;
+            if (au_slice == 0) {
+              continue;
+            }
             if (a.GetId() >= au_slice->Count())
               throw TFunctionFailedException(__OlxSourceInfo, "assert");
             TSAtom *sa = (*au_slice)[a.GetId()];
-            if (sa != NULL && !sa->IsDeleted())
+            if (sa != 0 && !sa->IsDeleted()) {
               rv.Add(sa);
+            }
           }
         }
       }
@@ -209,27 +220,37 @@ public:
   }
   //...........................................................................
   TSAtom* Find(const TSAtom::Ref& ref) const {
-    if( data == NULL )  return NULL;
+    if (data == 0) {
+      return 0;
+    }
     const vec3i t = smatd::GetT(ref.matrix_id);
-    if( !data->registry.IsInRange(t) ) return NULL;
+    if (!data->registry.IsInRange(t)) {
+      return 0;
+    }
     TArrayList<TSAtomPList*>* aum_slice = data->registry.Value(t);
-    if( aum_slice == NULL )  return NULL;
+    if (aum_slice == 0) {
+      return 0;
+    }
     TSAtomPList* au_slice = (*aum_slice)[smatd::GetContainerId(ref.matrix_id)];
     // the latter condition - if AU is the same but some objects use Q-peaks and
     // their number differs
-    if( au_slice == NULL || au_slice->Count() <= ref.catom_id)
-      return NULL;
+    if (au_slice == 0 || au_slice->Count() <= ref.catom_id) {
+      return 0;
+    }
     return (*au_slice)[ref.catom_id];
   }
   //...........................................................................
   TSBond* Find(const TSBond::Ref& ref) const {
     TSAtom* a = Find(ref.a);
-    if( a == NULL )  return NULL;
-    for( size_t i=0; i < a->BondCount(); i++ )  {
-      if( !a->Bond(i).IsDeleted() && a->Bond(i).Another(*a) == ref.b )
-        return &a->Bond(i);
+    if (a == 0) {
+      return 0;
     }
-    return NULL;
+    for (size_t i = 0; i < a->BondCount(); i++) {
+      if (!a->Bond(i).IsDeleted() && a->Bond(i).Another(*a) == ref.b) {
+        return &a->Bond(i);
+      }
+    }
+    return 0;
   }
 };
 
@@ -246,7 +267,6 @@ struct ASObjectProvider : public IOlxObject {
   {}
   virtual class TXFile* CreateXFile() = 0;
   virtual IOlxObject* Replicate() const = 0;
-
 };
 
 struct SObjectProvider : public ASObjectProvider {
@@ -262,7 +282,7 @@ struct SObjectProvider : public ASObjectProvider {
     delete &planes;
   }
   virtual TXFile *CreateXFile();
-  virtual IOlxObject* Replicate() const {  return new SObjectProvider();  }
+  virtual IOlxObject* Replicate() const { return new SObjectProvider(); }
 };
 
 EndXlibNamespace()
