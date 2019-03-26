db2e9aca77dcc3b0ca388a03f27527c88dff0df5
pcxod <pcxod@273b6b03-9602-4203-8103-cbef60d4472e>
2019-03-26 10:43:25 +0000

    fixing error handling and automatically converting olxstr container to Py_Object one

--- trunk/repository/olxvar.cpp
+++ trunk/repository/olxvar.cpp
@@ -11,154 +11,190 @@
 #include "egc.h"
 #include "efile.h"
 
-TOlxVars* TOlxVars::Instance = NULL;
+TOlxVars* TOlxVars::Instance = 0;
 #ifdef _PYTHON
 #include "pyext.h"
 char TOlxPyVar::svmn[] = "setValue";
 char TOlxPyVar::gvmn[] = "getValue";
 
-TOlxPyVar::TOlxPyVar(PyObject* obj)  {
-  Str = NULL;
-  Obj = NULL;
+TOlxPyVar::TOlxPyVar(PyObject* obj) {
+  Str = 0;
+  Obj = 0;
   InitObject(obj);
 }
 //.............................................................................
-TOlxPyVar::TOlxPyVar(const TOlxPyVar& oo)  {
+TOlxPyVar::TOlxPyVar(const TOlxPyVar& oo) {
   Type = oo.Type;
-  Str = (oo.Str == NULL) ? NULL : new olxstr(*oo.Str);
+  Str = (oo.Str == 0) ? 0 : new olxstr(*oo.Str);
   Obj = oo.Obj;
-  if( Obj != NULL )  Py_INCREF(Obj);
+  if (Obj != 0) {
+    Py_INCREF(Obj);
+  }
 }
 //.............................................................................
 TOlxPyVar::~TOlxPyVar() {
   olx_del_obj(Str);
-  if (Obj != NULL && Obj != Py_None)
+  if (Obj != 0 && Obj != Py_None) {
     Py_DECREF(Obj);
+  }
 }
 //.............................................................................
-void TOlxPyVar::InitObject(PyObject* obj)  {
-  if( Obj != NULL )  Py_DECREF(Obj);
+void TOlxPyVar::InitObject(PyObject* obj) {
+  if (Obj != 0) {
+    Py_DECREF(Obj);
+  }
   Obj = obj;
   Py_INCREF(obj);
 
   Type = potNone;
 
-  if( PyObject_HasAttrString(obj, gvmn) )  Type |= potHasGetter;
-  if( PyObject_HasAttrString(obj, svmn) )  Type |= potHasSetter;
+  if (PyObject_HasAttrString(obj, gvmn)) {
+    Type |= potHasGetter;
+  }
+  if (PyObject_HasAttrString(obj, svmn)) {
+    Type |= potHasSetter;
+  }
 
-  PyObject* defv = NULL;
+  PyObject* defv = 0;
   PyTypeObject *ot;
 
-  if( HasGet() )  {
-    defv = PyObject_CallMethod(Obj, gvmn, NULL);
+  if (HasGet()) {
+    defv = PyObject_CallMethod(Obj, gvmn, 0);
     ot = defv->ob_type;
   }
-  else
+  else {
     ot = obj->ob_type;
+  }
 
-  if( ot == &PyInt_Type )          Type |= potInt;
-  else if( ot == &PyBool_Type )    Type |= potBool;
-  else if( ot == &PyFloat_Type )   Type |= potFloat;
-  else if( ot == &PyString_Type )  Type |= potString;
-  else if( ot == &PyUnicode_Type ) Type |= potString;
-  else if( ot == &PyComplex_Type ) Type |= potComplex;
-  else  Type |= potUndefined;
+  if (ot == &PyInt_Type) {
+    Type |= potInt;
+  }
+  else if (ot == &PyBool_Type) {
+    Type |= potBool;
+  }
+  else if (ot == &PyFloat_Type) {
+    Type |= potFloat;
+  }
+  else if (ot == &PyString_Type) {
+    Type |= potString;
+  }
+  else if (ot == &PyUnicode_Type) {
+    Type |= potString;
+  }
+  else if (ot == &PyComplex_Type) {
+    Type |= potComplex;
+  }
+  else {
+    Type |= potUndefined;
+  }
 
-  if( defv != NULL )  Py_DECREF(defv);
+  if (defv != 0) {
+    Py_DECREF(defv);
+  }
 }
 //.............................................................................
 PyObject* TOlxPyVar::ObjectValue(PyObject* obj)  {
   return PyObject_HasAttrString(obj, svmn)
-    ? PyObject_CallMethod(obj, gvmn, NULL) : obj;
+    ? PyObject_CallMethod(obj, gvmn, 0) : obj;
 }
 //.............................................................................
-PyObject* TOlxPyVar::GetObjVal()  {
-  if( Obj != NULL )  {
-    PyObject *rv = HasGet() ? PyObject_CallMethod(Obj, gvmn, NULL) : Obj;
-    if( !HasGet() )
+PyObject* TOlxPyVar::GetObjVal() {
+  if (Obj != 0) {
+    PyObject *rv = HasGet() ? PyObject_CallMethod(Obj, gvmn, 0) : Obj;
+    if (!HasGet()) {
       Py_INCREF(rv);
+    }
     return rv;
   }
-  else  if( Str != NULL )
+  else  if (Str != 0) {
     return PythonExt::BuildString(*Str);
+  }
   else {
     return PythonExt::SetErrorMsg(PyExc_RuntimeError, __OlxSourceInfo,
       "Uninitialised object");
   }
 }
 //.............................................................................
-void TOlxPyVar::Set(PyObject* obj)  {
-  if( Obj == NULL && obj == Py_None )  {
-    Py_DECREF(PythonExt::SetErrorMsg(PyExc_TypeError, __OlxSourceInfo,
-      "A valid object is expected"));
+void TOlxPyVar::Set(PyObject* obj) {
+  if (Obj == 0 && obj == Py_None) {
+    PythonExt::SetErrorMsg(PyExc_TypeError, __OlxSourceInfo,
+      "A valid object is expected");
     return;
   }
-  if( Obj != NULL )  {
-    if( obj == Py_None )  {
-      if( !HasSet() )  {
-        Py_DECREF(PythonExt::SetErrorMsg(PyExc_TypeError, __OlxSourceInfo,
-          "Missing set method"));
+  if (Obj != 0) {
+    if (obj == Py_None) {
+      if (!HasSet()) {
+        PythonExt::SetErrorMsg(PyExc_TypeError, __OlxSourceInfo,
+          "Missing set method");
         return;
       }
     }
-    if( Obj->ob_type == obj->ob_type )  {
+    if (Obj->ob_type == obj->ob_type) {
       Py_DECREF(Obj);
       Py_INCREF(obj);
       Obj = obj;
     }
-    else if( !HasSet() )  // replace object type
+    else if (!HasSet()) { // replace object type
       InitObject(obj);
-    else  {
+    }
+    else {
       PyObject *rv = PyObject_CallMethod(Obj, svmn, "O", obj);
-      if( rv != NULL )  Py_DECREF(rv);
+      if (rv != 0) {
+        Py_DECREF(rv);
+      }
     }
   }
-  else  {
-    if( obj->ob_type == &PyString_Type || obj->ob_type == &PyUnicode_Type )
+  else {
+    if (obj->ob_type == &PyString_Type || obj->ob_type == &PyUnicode_Type) {
       *Str = PythonExt::ParseStr(obj);
-    else  {
-      Py_DECREF(PythonExt::SetErrorMsg(PyExc_RuntimeError, __OlxSourceInfo,
-        "Uninitialised object"));
-      return;
+    }
+    else {
+      olx_del_obj(Str);
+      Str = 0;
+      InitObject(obj);
     }
   }
 }
 //.............................................................................
-void TOlxPyVar::Set(const olxstr& str)  {
-  if( Obj == NULL )  {
-    if( Str == NULL )  Str = new olxstr(str);
-    else               *Str = str;
+void TOlxPyVar::Set(const olxstr& str) {
+  if (Obj == 0) {
+    if (Str == 0) {
+      Str = new olxstr(str);
+    }
+    else {
+      *Str = str;
+    }
   }
-  else  {
-    if( HasSet() )  {
+  else {
+    if (HasSet()) {
       PyObject *arg = PythonExt::BuildString(str);
       PyObject *rv = PyObject_CallMethod(Obj, svmn, "O", arg);
-      if( rv != NULL )  Py_DECREF(rv);
+      if (rv != 0) {
+        Py_DECREF(rv);
+      }
       Py_DECREF(arg);
     }
-    else  {  // replace a primitive value with new object
+    else {  // replace a primitive value with new object
       PyObject * arg;
-      if( (Type & potBool) != 0  )  {
+      if ((Type & potBool) != 0) {
         bool v = str.Equalsi(TrueString());
-        if( !v && !str.Equalsi(FalseString()) )  {
+        if (!v && !str.Equalsi(FalseString())) {
           olxstr err(olxstr("Boolean is expected, got '") << str << '\'');
-          Py_DECREF(
-            PythonExt::SetErrorMsg(PyExc_TypeError, __OlxSourceInfo, err));
+          PythonExt::SetErrorMsg(PyExc_TypeError, __OlxSourceInfo, err);
           throw TInvalidArgumentException(__OlxSourceInfo, err);
         }
         arg = PyBool_FromLong(v);
       }
-      else if( (Type & potInt) != 0  )  {
-        if( !str.IsNumber() )  {
+      else if ((Type & potInt) != 0) {
+        if (!str.IsNumber()) {
           olxstr err(olxstr("Integer is expected, got '") << str << '\'');
-          Py_DECREF(
-            PythonExt::SetErrorMsg(PyExc_TypeError, __OlxSourceInfo, err));
+          PythonExt::SetErrorMsg(PyExc_TypeError, __OlxSourceInfo, err);
           throw TInvalidArgumentException(__OlxSourceInfo, err);
         }
         arg = Py_BuildValue("i", str.ToInt());
       }
-      else if( (Type & potFloat) != 0 )  {
-        if( !str.IsNumber() )  {
+      else if ((Type & potFloat) != 0) {
+        if (!str.IsNumber()) {
           olxstr err(olxstr("Float is expected, got '") << str << '\'');
           Py_DECREF(
             PythonExt::SetErrorMsg(PyExc_TypeError, __OlxSourceInfo, err));
@@ -166,12 +202,12 @@ void TOlxPyVar::Set(const olxstr& str)  {
         }
         arg = Py_BuildValue("d", str.ToDouble());
       }
-      else if( (Type & potString) != 0 )
+      else if ((Type & potString) != 0) {
         arg = PythonExt::BuildString(str);
-      else  {
+      }
+      else {
         olxstr err(olxstr("String is expected, got '") << str << '\'');
-        Py_DECREF(
-          PythonExt::SetErrorMsg(PyExc_TypeError, __OlxSourceInfo, err));
+        PythonExt::SetErrorMsg(PyExc_TypeError, __OlxSourceInfo, err);
         throw TInvalidArgumentException(__OlxSourceInfo, err);
       }
       Py_DECREF(Obj);
@@ -180,26 +216,33 @@ void TOlxPyVar::Set(const olxstr& str)  {
   }
 }
 //.............................................................................
-const olxstr& TOlxVars::GetVarStr(size_t index)  {
+const olxstr& TOlxVars::GetVarStr(size_t index) {
   volatile olx_scope_cs cs(CS());
   TOlxPyVar& oo = Instance->Vars.GetValue(index);
-  if( oo.GetStr() != NULL )  return *oo.GetStr();
+  if (oo.GetStr() != 0) {
+    return *oo.GetStr();
+  }
   PyObject *po = oo.GetObjVal();
   double fv;
-  if( po->ob_type == &PyString_Type || po->ob_type == &PyUnicode_Type )
+  if (po->ob_type == &PyString_Type || po->ob_type == &PyUnicode_Type) {
     return TEGC::Add(new olxstr(PythonExt::ParseStr(po)));
-  else if( po->ob_type == &PyFloat_Type && PyArg_Parse(po, "d", &fv) )
+  }
+  else if (po->ob_type == &PyFloat_Type && PyArg_Parse(po, "d", &fv)) {
     return TEGC::Add(new olxstr(fv));
-  else
+  }
+  else {
     return TEGC::Add(new olxstr(PyObject_REPR(po)));
+  }
 }
 //.............................................................................
-TOlxVars::TOlxVars()  {
-  if (Instance != NULL)
+TOlxVars::TOlxVars() {
+  if (Instance != 0) {
     throw TFunctionFailedException(__OlxSourceInfo, "singleton");
+  }
   Instance = this;
   OnVarChange = &Actions.New("OnVarChange");
-//  TEGC::AddP(this);  // we cannot do this, as Pyhon might be Finalised beforehand!!!
+  // we cannot do this, as Pyhon might be Finalised beforehand!!!
+  //  TEGC::AddP(this);
 }
 #endif // _PYTHON
 //............................................................................
@@ -233,13 +276,15 @@ void olxvar_funFlush(const TStrObjList& Params, TMacroData &E) {
   Wildcard wc(Params[1]);
   for (size_t i=0; i < TOlxVars::VarCount(); i++) {
     olxstr vn = TOlxVars::GetVarName(i);
-    if (!wc.DoesMatch(vn) || vn.Length() < from) continue;
+    if (!wc.DoesMatch(vn) || vn.Length() < from) {
+      continue;
+    }
     settings.Add(vn.SubStringFrom(from)) << '=' << TOlxVars::GetVarStr(i);
   }
   TEFile::WriteLines(Params[0], TCStrList(settings));
 }
 TLibrary *TOlxVars::ExportLibrary(const olxstr &name, TLibrary *_l) {
-  TLibrary *l = _l == NULL ? new TLibrary(name) : _l;
+  TLibrary *l = _l == 0 ? new TLibrary(name) : _l;
   l->Register(
     new TStaticFunction(&olxvar_funSetVar,
       "SetVar", fpTwo, "Sets the value of the specified variable"));
