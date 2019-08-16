dd54910461b7abece0e203a45c0e9266ce7d25cd
pcxod <pcxod@273b6b03-9602-4203-8103-cbef60d4472e>
2019-08-16 12:42:44 +0000

    - making it compatible with wx3.0 on Linux...

--- olex/mainform.cpp
+++ olex/mainform.cpp
@@ -1172,8 +1172,15 @@ void TMainForm::XApp(Olex2App *XA)  {
   XLibMacros::OnDelIns().Add(this, ID_DELINS, msiExit);
   XLibMacros::OnAddIns().Add(this, ID_ADDINS, msiExit);
   LoadVFS(plGlobal);
-
-  HtmlManager.InitialiseMain(wxBORDER_NONE|wxCLIP_CHILDREN|wxALWAYS_SHOW_SB);
+  {
+    long flags = wxBORDER_NONE | wxCLIP_CHILDREN;
+#if wxMAJOR_VERSION == 3 && wxMINOR_VERSION == 0
+    flags |= wxVSCROLL;
+#else
+    flags != wxALWAYS_SHOW_SB;
+#endif
+    HtmlManager.InitialiseMain(flags);
+  }
   GetLibrary().AttachLibrary(HtmlManager.ExportLibrary());
 
   HtmlManager.OnLink.Add(this, ID_ONLINK);
