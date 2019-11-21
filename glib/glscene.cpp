a4b07cf8fa661eaa9de7375f20b318038b2f3c41
pcxod <oleg_dolomanov@hotmail.com>
2019-11-21 12:34:30 +0000

    - synchronising the interface + some refactoring

--- glib/glscene.cpp
+++ glib/glscene.cpp
@@ -24,11 +24,12 @@ bool AGlScene::StartDraw() {
   return true;
 }
 //.............................................................................
-void AGlScene::EndDraw()  {  olx_gl::flush();  }
+void AGlScene::EndDraw() { olx_gl::flush(); }
 //.............................................................................
 bool AGlScene::StartSelect(int x, int y, GLuint *Bf) {
-  if (!MakeCurrent())
+  if (!MakeCurrent()) {
     return false;
+  }
   olx_gl::selectBuffer(MAXSELECT, Bf);
   olx_gl::renderMode(GL_SELECT);
   olx_gl::initNames();
@@ -37,7 +38,7 @@ bool AGlScene::StartSelect(int x, int y, GLuint *Bf) {
   return true;
 }
 //.............................................................................
-int AGlScene::EndSelect()  {
+int AGlScene::EndSelect() {
   int hits = olx_gl::renderMode(GL_RENDER);
   olx_gl::flush();
   FParent->SetView(false, 1);
@@ -47,11 +48,11 @@ int AGlScene::EndSelect()  {
 TGlFont& AGlScene::CreateFont(const olxstr& name, const olxstr& fntDescription)
 {
   const size_t i = FontsDict.IndexOf(name);
-  if( i != InvalidIndex ) {
+  if (i != InvalidIndex) {
     // overwrite
     FontsDict.GetValue(i)->ClearData();
     FontsDict.GetValue(i)->SetIdString(fntDescription);
-    if( olx_is_valid_index(FontsDict.GetValue(i)->SmallId) )  {
+    if (olx_is_valid_index(FontsDict.GetValue(i)->SmallId)) {
       SmallFonts[FontsDict.GetValue(i)->SmallId]->ClearData();
       SmallFonts[FontsDict.GetValue(i)->SmallId]->SetIdString(fntDescription);
     }
@@ -60,7 +61,7 @@ TGlFont& AGlScene::CreateFont(const olxstr& name, const olxstr& fntDescription)
   TGlFont* fnt = Fonts.Add(new TGlFont(*this, Fonts.Count(), name));
   fnt->SetIdString(fntDescription);
   FontsDict.Add(name, fnt);
-  if( !fnt->IsVectorFont() )  {
+  if (!fnt->IsVectorFont()) {
     fnt->SmallId = SmallFonts.Count();
     SmallFonts.Add(
       new TGlFont(*this, SmallFonts.Count(), name))->SetIdString(
@@ -120,23 +121,27 @@ const_strlist AGlScene::ToPov() const {
   TGlOption cl_clear = FParent->LightModel.GetClearColor();
   out.Add("background { color ") << pov::to_str(cl_clear) << " }";
   out.Add("camera {");
-  out.Add(" location <0,0,") << 3./FParent->GetBasis().GetZoom() << '>';
+  out.Add(" location <0,0,") << 3. / FParent->GetBasis().GetZoom() << '>';
   out.Add(" angle 25");
   out.Add(" up 1");
   out.Add(" right -4/3");
   out.Add(" look_at <0,0,0>");
   out.Add("}");
-  for( size_t i=0; i < 8; i++ )  {
+  for (size_t i = 0; i < 8; i++) {
     const TGlLight &l = FParent->LightModel.GetLight(i);
-    if( !l.IsEnabled() )  continue;
+    if (!l.IsEnabled()) {
+      continue;
+    }
     out.Add("light_source {");
     TGlOption lp = l.GetPosition();
     out.Add(" ") << pov::to_str(lp, false);
     lp = l.GetDiffuse();
-    if( lp.IsEmpty() )
+    if (lp.IsBlack()) {
       lp = l.GetAmbient();
-    if( lp.IsEmpty() )
+    }
+    if (lp.IsBlack()) {
       lp = l.GetSpecular();
+    }
     lp *= 1.5;
     out.Add(" color ") << pov::to_str(lp);
     out.Add("}");
@@ -150,61 +155,71 @@ olxstr AGlScene::MetaFont::BuildOlexFontId(const olxstr& fileName, short size,
   bool fixed, bool bold, bool italic)
 {
   olxstr prefix, suffix;
-  if( !fileName.IsEmpty() )
+  if (!fileName.IsEmpty())
     prefix << '#' << fileName << ':';
-  suffix << (fixed ? "f" : "n" );
-  if( italic )  {
+  suffix << (fixed ? "f" : "n");
+  if (italic) {
     suffix << "i";
-    if( bold )
+    if (bold) {
       suffix << "b";
+    }
   }
-  else if( bold )
+  else if (bold) {
     suffix << "rb";
-  else
+  }
+  else {
     suffix << "r";
+  }
   suffix.RightPadding(4, '_');
   return prefix << suffix << size;
 }
 //.............................................................................
-bool AGlScene::MetaFont::SetIdString(const olxstr& idstr)  {
+bool AGlScene::MetaFont::SetIdString(const olxstr& idstr) {
   OriginalId = idstr;
-  if( IsOlexFont(idstr) )  {
+  if (IsOlexFont(idstr)) {
     size_t ci = idstr.IndexOf(':');
-    if( ci == InvalidIndex )
+    if (ci == InvalidIndex) {
       throw TFunctionFailedException(__OlxSourceInfo, "invalid font ID");
-    FileName = idstr.SubStringFrom(1).SubStringTo(ci-1);
-    olxstr fntid( idstr.SubStringFrom(ci+1) );
-    if( fntid.Length() < 5 )
+    }
+    FileName = idstr.SubStringFrom(1).SubStringTo(ci - 1);
+    olxstr fntid(idstr.SubStringFrom(ci + 1));
+    if (fntid.Length() < 5) {
       throw TFunctionFailedException(__OlxSourceInfo, "invalid font ID");
+    }
     Fixed = (fntid.CharAt(0) == 'f');
     Italic = (fntid.CharAt(1) == 'i');
     Bold = (fntid.CharAt(2) == 'b');
     Size = fntid.SubStringFrom(4).ToInt();
   }
-  else if( IsVectorFont(idstr) )  {
+  else if (IsVectorFont(idstr)) {
     Fixed = false;
     Italic = false;
     Bold = false;
     Size = 15;
-    if( idstr.SubStringFrom(1).IsNumber() )
+    if (idstr.SubStringFrom(1).IsNumber()) {
       Size = idstr.SubStringFrom(1).ToInt();
+    }
   }
-  else
+  else {
     return false;
+  }
   return true;
 }
 //.............................................................................
 olxstr AGlScene::MetaFont::GetIdString() const {
-  if( IsOlexFont(OriginalId) )
+  if (IsOlexFont(OriginalId)) {
     return BuildOlexFontId(FileName, Size, Fixed, Bold, Italic);
-  if( IsVectorFont(OriginalId) )
+  }
+  if (IsVectorFont(OriginalId)) {
     return olxstr('@') << Size;
+  }
   return EmptyString();
 }
 //.............................................................................
 olxstr AGlScene::MetaFont::GetFileIdString() const {
-  if( IsOlexFont(OriginalId) )
+  if (IsOlexFont(OriginalId)) {
     return BuildOlexFontId(EmptyString(), Size, Fixed, Bold, Italic);
+  }
   throw TInvalidArgumentException(__OlxSourceInfo, "Olex2 font is expected");
 }
 //.............................................................................
@@ -216,10 +231,10 @@ void AGlScene::LibMakeCurrent(TStrObjList& Cmds, const TParamList& Options,
   MakeCurrent();
 }
 //.............................................................................
-TLibrary* AGlScene::ExportLibrary(const olxstr& name)  {
+TLibrary* AGlScene::ExportLibrary(const olxstr& name) {
   TLibrary* lib = new TLibrary(name.IsEmpty() ? olxstr("scene") : name);
   lib->Register(
-    new TMacro<AGlScene>(this,  &AGlScene::LibMakeCurrent, "MakeCurrent",
+    new TMacro<AGlScene>(this, &AGlScene::LibMakeCurrent, "MakeCurrent",
       EmptyString(), fpNone,
       "Make scene for rendering/updates")
   );
